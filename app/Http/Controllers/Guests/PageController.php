ac30443a5d06dceb751e465eab7ab9ff1f9bb62e
FedericaSidoti <federicasidoti21@gmail.com>
2023-12-12 17:02:03 +0100

    ADD-model Movie

--- app/Http/Controllers/Guests/PageController.php
+++ app/Http/Controllers/Guests/PageController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers\Guests;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index() 
     {
-        
-        return view('index');
+        $movies= Movie::all();
+        dd($movies);
+        return view('index', compact($movies));
     }
 }
 
