89f1f7f1da0129a953d737e91c6f0e4e22519b66
FedericaSidoti <federicasidoti21@gmail.com>
2023-12-12 16:32:03 +0100

    ADD-PageController

---
+++ app/Http/Controllers/Guests/PageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Guests;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index() 
+    {
+        
+        return view('index');
+    }
+}
+
